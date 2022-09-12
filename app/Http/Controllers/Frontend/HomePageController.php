3d7896de2820a7e94c435afc363cb59f73d70a31
billdevmaster71 <billdevmaster@gmail.com>
2022-09-12 23:08:54 +0300

    fix: not found user

--- app/Http/Controllers/Frontend/HomePageController.php
+++ app/Http/Controllers/Frontend/HomePageController.php
@@ -11,6 +11,7 @@ use App\Models\LocationPesuboxs;
 use App\Models\Orders;
 use App\Models\Bookings;
 use App\Models\Services;
+use App\Models\User;
 use Illuminate\Support\Facades\Mail;
 use App\Mail\BookIdMail;
 // use Illuminate\Support\Facades\Input;
